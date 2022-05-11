e5a51749e6538a2503675df6057af9c7a6ccd59d
ImaWumpus <cadigour@gmail.com>
2022-05-11 13:46:41 +0200

    présision comp

--- competences.php
+++ competences.php
@@ -14,93 +14,96 @@ include 'header.php';
     <br>
     <span class="tb">
     Compétences mobilisées :<br>
-    1.1.2 <br>
-    1.2.3 <br>
-    1.4.2 <br>
-    1.5.1 <br>
-    1.6.1 <br>
-    <a href="file/Déploiement-appli.docx">Element de preuve</a><br>
+    Recenser et identifier les ressources numériques : 1.1.2 <br>
+    Traiter les demandes concernant les applications : 1.2.3 <br>
+    Planifier les activités : 1.4.2 <br>
+    Réaliser les test d'intégration et d'acceptation d'un service : 1.5.1 <br>
+    Mettre en place son environnement d'apprentissage personnel : 1.6.1 <br>
     </span>
     <br>
     <h3>Gerer son identitée personnelle (portfolio) :</h3>
     <br>
     <span class="tb">
     Compétences mobilisées :<br>
-    1.6.3<br>
+    Mettre en place son environnement d'apprentissage personnel : 1.6.1 <a href="https://github.com/ImaWumpus/PortfolioV2">Element de preuve</a><br>
+    Gérer son identité professionnelle : 1.6.3<br>
     </span>
     <br>
     <h3>Déploiement d'une appliquation sur un serveur web :</h3>
     <br><span class="tb">
-    1.1.2 1.2.1 1.4.2
-    <br></span>
+    Exploiter des référentiels, normes et standards adoptés par le préstataire informatique : 1.1.2 <br>
+    Collecter, suivre et orienter des demandes : 1.2.1 <br>
+    Planifier les activités : 1.4.2 <br>
+    <a href="file/Déploiement-appli.docx">Element de preuve</a><br>
+    </span>
     <br>
     <h3>Veille informatique : </h3>
     <br><span class="tb">
     Compétences mobilisées :<br>
-    1.6.2 <a href="veille-info.php">Element de preuve</a><br>
+    Mettre en oeuvre des outils et stratégies de veille informationnelle : 1.6.2 <a href="veille-info.php">Element de preuve</a><br>
     <br></span>
     <h3>Tableau comparatif : </h3> 
     <br><span class="tb">
     Compétences mobilisées :<br>
-    1.1.1 <a href="file/tableau-comparatif-V2.xlsx">Element de preuve</a><br>
+    Recenser et identifier les ressources numériques : 1.1.1 <a href="file/tableau-comparatif-V2.xlsx">Element de preuve</a><br>
     <br></span>
     <h3>Glossaire :</h3>
     <br><span class="tb">
     Compétences mobilisées :<br>
-    1.1.1 <a href="file/Glossaire.docx">Element de preuve</a><br>
+    Recenser et identifier les ressources numériques : 1.1.1 <a href="file/Glossaire.docx">Element de preuve</a><br>
     </span>
     <br>
     <h3>Mise en œuvre de la maquette :</h3>
     <br>
     <span class="tb">
         Compétences mobilisées :<br>
-        1.1.1 <br>
-        1.3.3 <a href="file/labscontrol.rar">Element de preuve</a><br>
-        1.5.1 <a href="file/Mise en oeuvre maquette.docx">Element de preuve</a><br>
-        1.6.3 <a href="file/CV Mael Cadiou.pdf">Element de preuve</a><br>
+        Recenser et identifier les ressources numériques : 1.1.1 <br>
+        Participer à l'évolution d'un site web exploitant les données de l'organisation : 1.3.3 <a href="file/labscontrol.rar">Element de preuve</a><br>
+        Réaliser les test d'intégration et d'acceptation d'un service : 1.5.1 <a href="file/Mise en oeuvre maquette.docx">Element de preuve</a><br>
+        Gérer son identité professionnelle : 1.6.3 <a href="file/CV Mael Cadiou.pdf">Element de preuve</a><br>
     <br></span>
     <br>
     <h3>Tableau comparatif entre les requetes GET et POST :</h3>
     <br>
     <span class="tb">
         Compétences mobilisées :<br>
-        1.1.1 <a href="file/GET et POST.docx">Element de preuve</a><br>
+        Recenser et identifier les ressources numériques : 1.1.1 <a href="file/GET et POST.docx">Element de preuve</a><br>
     </span>
     <br>
     <h3>Formation GIT :</h3>
     <br>
     <span class="tb">
         Compétences mobilisées :<br>
-        1.1.1 <br>
-        1.1.2<br>
-        1.1.5<br>
-        1.4.2 <br>
-        1.6.1 <br>
+        Recenser et identifier les ressources numériques : 1.1.1 <br>
+        Exploiter des référentiels, normes et standards adoptés par le préstataire informatique : 1.1.2<br>
+        Gérer des sauvegardes : 1.1.5<br>
+        Planifier les activités : 1.4.2 <br>
+        Mettre en place son environnement d'apprentissage personnel : 1.6.1 <br>
         <a href="file/Formation Gitlab.zip">Element de preuve</a><br>
     </span>
     <br>
     <h3>Documentation sur les logiciel/application/framework utilisés : </h3>
     <br>
     <span>
-    1.1.1
+    Recenser et identifier les ressources numériques : 1.1.1
     <br></span>
     <br>
     <h3>Test d'uri sur postman  :</h3>
     <br>
     <span class="tb">
-    1.1.1 <a href="file/postman.png">Element de preuve</a><br>
+    Recenser et identifier les ressources numériques : 1.1.1 <a href="file/postman.png">Element de preuve</a><br>
     <br></span>
     <br>
     <h3>Application web affichant les matériels prétés via une bdd glpi et son api :</h3>
     <br>
     <span class="tb">
         Compétences mobilisées :<br>
-        1.1.2 <br>
-        1.2.2 <br>
+        Exploiter des référentiels, normes et standards adoptés par le préstataire informatique : 1.1.2 <br>
+        Traiter des demandes concernant les services réseau et systeme, applicatifs : 1.2.2 <br>
         <a href="file/mes equipements.zip">Element de preuve</a><br>
-        1.4.2 <a href="file/20220128_094825.jpg">Element de preuve</a><br>
-        1.5.1 <br>
-        1.6.3
+        Planifier les activités : 1.4.2 <a href="file/20220128_094825.jpg">Element de preuve</a><br>
+        Réaliser les test d'intégration et d'acceptation d'un service : 1.5.1 <br>
+        Gérer son identité professionnelle : 1.6.3
     </span>
 
 </div>
